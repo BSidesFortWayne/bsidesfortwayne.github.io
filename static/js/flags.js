33fde958ee5cbb02e8cf63244453ceb37903bc17
bsears <bry.j.sears@gmail.com>
2026-01-11 14:27:46 -0500

    CTF Flags

---
+++ static/js/flags.js
@@ -0,0 +1,3 @@
+console.log("Encrypted Flags loaded");
+console.log("Free Ticket: e808cbc5e0ca3690e026f2c4b4d644af9a2179f75bfd6038626d37563e75d2f4");
+console.log("CTF Flag: 5f80172b36b163318210d8223dbc4d8432006e4934a445e0451118b51fea3c3d");  
